e7c4dbcceff5ff5d22a048de2d33f7555fb94c63
Kevin Brousse <kbrousse@2D5.42angouleme.fr>
2022-06-02 18:23:10 +0200

    Working so_long with one random p and one random e

--- src/change_matrix.c
+++ src/change_matrix.c
@@ -16,19 +16,25 @@ void	change_matrix(t_game *g)
 {
 	int	y;
 	int	x;
-	int	count;
-	int	random;
+	int	count_p;
+	int	count_e;
+	int	random_p;
+	int	random_e;
 	
 	srand(time(NULL));
-	random = (int)(rand() / (double)RAND_MAX * (g->map->nb_starts - 1));
+	random_p = (int)(rand() / (double)RAND_MAX * (g->map->nb_starts - 1));
+	random_e = (int)(rand() / (double)RAND_MAX * (g->map->nb_esc - 1));
 	y = -1;
-	count = 0;
+	count_p = 0;
+	count_e = 0;
 	while (++y < g->map->y)
 	{
 		x = -1;
 		while (++x < g->map->x)
 		{
-			if (g->map->matrix[y][x] == 'P' && count++ != random)
+			if (g->map->matrix[y][x] == 'P' && count_p++ != random_p)
+				g->map->matrix[y][x] = '0';
+			else if (g->map->matrix[y][x] == 'E' && count_e++ != random_e)
 				g->map->matrix[y][x] = '0';
 				
 		}
